c17342e890a31f1797a47e0761b7306420d58fd3
David Vargas <dvarrui@protonmail.com>
2026-04-23 11:57:54 +0100

    refactor

--- lib/dsl/clang.rb
+++ lib/dsl/clang.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 require_relative "clang/version"
+require_relative "clang/clang"
 
-module Dsl
-  module Clang
-    class Error < StandardError; end
-    # Your code goes here...
-  end
+# module Dsl
+#   module Clang
+#     class Error < StandardError; end
+#     # Your code goes here...
+#   end
+# end
+
+def main(&block)
+  modern_c = CLang.new
+  modern_c.run(block)
 end
